9a6baad089f40a6b98d4273f5c6b057f1ddfcc2e
U2FsdGVkX1 <U2FsdGVkX1@gmail.com>
2017-04-17 18:40:42 +0800

    promise

--- modee.js
+++ modee.js
@@ -1,26 +1,26 @@
 mysql = require('mysql');
 
 class Modee {
-    constructor(callback, options) {
+    constructor (callback, options) {
         this.db = mysql.createConnection(options);
         this.db.connect(callback);
     }
-    data_t(key, value) {
+    data_t (key, value) {
         var result = '';
         var value = this.value_quote(value);
         var valueMerge = value;
         var valueObject = typeof value == 'object';
-        if(valueObject == true) {
+        if (valueObject == true) {
             valueMerge = value.join(',');
         }
 
-        if(key == 'LIMIT') {
+        if (key == 'LIMIT') {
             result = 'LIMIT ' + valueMerge;
         } else {
-            var flag = key.match(/(\w+)(\[(.{1,2})\])?/);
+            let flag = key.match(/(\w+)(\[(.{1,2})\])?/);
             key = this.column_quote(flag[1]);
             flag = flag[3];
-            switch(flag) {
+            switch (flag) {
                 case '>':
                     result = key + ' > ' + valueMerge;
                     break;
@@ -34,7 +34,7 @@ class Modee {
                     result = key + ' <= ' + valueMerge;
                     break;
                 case '!':
-                    if(valueObject == true) {
+                    if (valueObject == true) {
                         result = key + ' NOT IN (' + valueMerge + ')';
                     } else {
                         result = key + ' != ' + valueMerge;
@@ -59,7 +59,7 @@ class Modee {
                     result = key + ' = ' + key + ' / ' + valueMerge;
                     break;
                 case undefined:
-                    if(valueObject == true) {
+                    if (valueObject == true) {
                         result = key + ' IN (' + valueMerge + ')';
                     } else {
                         result = key + ' = ' + valueMerge;
@@ -70,25 +70,25 @@ class Modee {
 
         return result;
     }
-    where_t(where, operator = ' ') {
+    where_t (where, operator = ' ') {
         var result = [];
-        if(operator == ' ') {
-            var wherePush = false;
-            for(var key in where) {
-                if(
+        if (operator == ' ') {
+            let wherePush = false;
+            for (var key in where) {
+                if (
                     key != 'LIMIT'
                 ) {
                     wherePush = true;
                     break;
                 }
             }
-            if(wherePush == true) {
+            if (wherePush == true) {
                 result.push('WHERE');
             }
         }
 
-        for(var key in where) {
-            if(key == 'AND' || key == 'OR') {
+        for (var key in where) {
+            if (key == 'AND' || key == 'OR') {
                 result.push('(' + this.where_t(where[key], ' ' + key + ' ') + ')');
             } else {
                 result.push(this.data_t(key, where[key]));
@@ -97,14 +97,14 @@ class Modee {
 
         return result.join(operator);
     }
-    column_quote(column) {
+    column_quote (column) {
         var result = [];
-        switch(typeof column) {
+        switch (typeof column) {
             case 'string':
                 result = '`' + column + '`';
                 break;
             case 'object':
-                for(var index in column) {
+                for (var index in column) {
                     result[index] = this.column_quote(column[index]);
                 }
                 break;
@@ -112,9 +112,9 @@ class Modee {
 
         return result;
     }
-    value_quote(value) {
+    value_quote (value) {
         var result = [];
-        switch(typeof value) {
+        switch (typeof value) {
             case 'number':
                 result = value;
                 break;
@@ -126,7 +126,7 @@ class Modee {
                 result = value == true ? 1 : 0
                 break;
             case 'object':
-                for(var index in value) {
+                for (var index in value) {
                     result[index] = this.value_quote(value[index]);
                 }
                 break;
@@ -137,139 +137,123 @@ class Modee {
 
         return result;
     }
-    query(callback, sql) {
-        this.db.query(sql, callback);
-        return {
-            sql: sql
-        };
+    query (sql) {
+        return new Promise((resolve, reject) => {
+            this.db.query(sql, (err, results, fields) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve([results, fields, sql]);
+                }
+            });
+        });
     }
-    select(callback, table, column = '*', where = []) {
-        if(column != '*') {
+    select (table, column = '*', where = []) {
+        if (column != '*') {
             column = this.column_quote(column);
-            if(typeof column == 'object') {
+            if (typeof column == 'object') {
                 column = column.join(',');
             }
         }
 
         var sql = 'SELECT ' + column + ' FROM ' + this.column_quote(table) + ' ' + this.where_t(where);
-        return this.query(callback, sql);
+        return this.query(sql);
     }
-    insert(callback, table, data) {
-        if(typeof data[0] == 'undefined') {
+    insert (table, data) {
+        if (typeof data[0] == 'undefined') {
             data = [data];
         }
 
         var dataPush = [];
-        for(var index in data) {
+        for (var index in data) {
             dataPush.push('(' + this.value_quote(Object.values(data[index])) + ')');
         }
 
         var sql = 'INSERT INTO ' + this.column_quote(table) + '(' + this.column_quote(Object.keys(data[0])) + ') VALUES ' + dataPush.join(',');
-        return this.query(callback, sql);
+        return this.query(sql);
     }
-    update(callback, table, data, where = []) {
+    update (table, data, where = []) {
         var dataPush = [];
-        for(var key in data) {
+        for (var key in data) {
             dataPush.push(this.data_t(key, data[key]));
         }
 
         var sql = 'UPDATE ' + this.column_quote(table) + ' SET ' + dataPush.join(',') + ' ' + this.where_t (where);
-        return this.query(callback, sql);
+        return this.query(sql);
     }
-    delete(callback, table, where) {
+    delete (table, where) {
         var sql = 'DELETE FROM ' + this.column_quote(table) + ' ' + this.where_t (where);
-        return this.query(callback, sql);
+        return this.query(sql);
     }
-    get(callback, table, column = '*', where = []) {
-        if(column != '*') {
+    get (table, column = '*', where = []) {
+        if (column != '*') {
             var columnNQ = column; 
             var columnObject = typeof column == 'object';
 
             column = this.column_quote(column);
-            if(columnObject == true) {
+            if (columnObject == true) {
                 column = column.join(',');
             }
         }
 
         where['LIMIT'] = 1;
         var sql = 'SELECT ' + column + ' FROM ' + this.column_quote(table) + ' ' + this.where_t(where);
-        return this.query(function(err, rows, fields) {
-            if(err) {
-                callback(err, rows, fields);
+        return this.query(sql).then((tmp) => {
+            let [results, fields, sql] = tmp;
+            if (columnObject == false) {
+                return [results[0][columnNQ], fields, sql];
             } else {
-                if(columnObject == false) {
-                    callback(err, rows[0][columnNQ], fields);
-                } else {
-                    callback(err, rows[0], fields);
-                }
+                return [results[0], fields, sql];
             }
-        }, sql);
+        });
     }
-    has(callback, table, where) {
+    has (table, where) {
         var sql = 'SELECT NULL FROM ' + this.column_quote(table) + ' ' + this.where_t(where);
         sql = 'SELECT EXISTS(' + sql + ') AS ' + this.column_quote('result');
-        return this.query(function(err, rows, fields) {
-            if(err) {
-                callback(err, rows, fields);
-            } else {
-                callback(err, rows[0]['result'] == 1, fields);
-            }
-        }, sql);
+        return this.query(sql).then((tmp) => {
+            let [results, fields, sql] = tmp;
+            return [results[0]['result'] == 1, fields, sql];
+        });
     }
-    count(callback, table, where = []) {
+    count (table, where = []) {
         var sql = 'SELECT COUNT(*) FROM ' + this.column_quote(table) + ' ' + this.where_t(where);
         sql = 'SELECT (' + sql + ') AS ' + this.column_quote('result');
-        return this.query(function(err, rows, fields) {
-            if(err) {
-                callback(err, rows, fields);
-            } else {
-                callback(err, rows[0]['result'], fields);
-            }
-        }, sql);
+        return this.query(sql).then((tmp) => {
+            let [results, fields, sql] = tmp;
+            return [results[0]['result'], fields, sql];
+        });
     }
-    max(callback, table, column, where = []) {
+    max (table, column, where = []) {
         var sql = 'SELECT MAX(' + this.column_quote(column) + ') FROM ' + this.column_quote(table) + ' ' + this.where_t(where);
         sql = 'SELECT (' + sql + ') AS ' + this.column_quote('result');
-        return this.query(function(err, rows, fields) {
-            if(err) {
-                callback(err, rows, fields);
-            } else {
-                callback(err, rows[0]['result'], fields);
-            }
-        }, sql);
+        return this.query(sql).then((tmp) => {
+            let [results, fields, sql] = tmp;
+            return [results[0]['result'], fields, sql];
+        });
     }
-    min(callback, table, column, where = []) {
+    min (table, column, where = []) {
         var sql = 'SELECT MIN(' + this.column_quote(column) + ') FROM ' + this.column_quote(table) + ' ' + this.where_t(where);
         sql = 'SELECT (' + sql + ') AS ' + this.column_quote('result');
-        return this.query(function(err, rows, fields) {
-            if(err) {
-                callback(err, rows, fields);
-            } else {
-                callback(err, rows[0]['result'], fields);
-            }
-        }, sql);
+        return this.query(sql).then((tmp) => {
+            let [results, fields, sql] = tmp;
+            return [results[0]['result'], fields, sql];
+        });
     }
-    avg(callback, table, column, where = []) {
+    avg (table, column, where = []) {
         var sql = 'SELECT AVG(' + this.column_quote(column) + ') FROM ' + this.column_quote(table) + ' ' + this.where_t(where);
         sql = 'SELECT (' + sql + ') AS ' + this.column_quote('result');
-        return this.query(function(err, rows, fields) {
-            if(err) {
-                callback(err, rows, fields);
-            } else {
-                callback(err, rows[0]['result'], fields);
-            }
-        }, sql);
+        return this.query(sql).then((tmp) => {
+            let [results, fields, sql] = tmp;
+            return [results[0]['result'], fields, sql];
+        });
     }
-    sum(callback, table, column, where = []) {
+    sum (table, column, where = []) {
         var sql = 'SELECT SUM(' + this.column_quote(column) + ') FROM ' + this.column_quote(table) + ' ' + this.where_t(where);
         sql = 'SELECT (' + sql + ') AS ' + this.column_quote('result');
-        return this.query(function(err, rows, fields) {
-            if(err) {
-                callback(err, rows, fields);
-            } else {
-                callback(err, rows[0]['result'], fields);
-            }
-        }, sql);
+        return this.query(sql).then((tmp) => {
+            let [results, fields, sql] = tmp;
+            return [results[0]['result'], fields, sql];
+        });
     }
 }
 module.exports = Modee;
